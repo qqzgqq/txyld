c1b4cdc979bd0aebfaa73b06f984d05532abd4d2
zengguang <zengg@tongxinyiliao.com>
2018-01-17 17:45:53 +0800

    go+iris网站

--- main.go
+++ main.go
@@ -9,11 +9,13 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/kataras/iris"
+	"github.com/kataras/iris/sessions"
 )
 
 // TxlydSQL mysql表txldy
 type TxlydSQL struct {
-	IDsql        int    `db:"id"`
+	IDsql int `db:"id"`
+
 	Starttime    string `db:"starttime"`
 	Endtime      string `db:"endtime"`
 	Question     string `db:"question"`
@@ -33,17 +35,40 @@ type MobaN struct {
 	CountList        string `db:"count_list"`
 }
 
+// ZhuCe mysql表
+type ZhuCe struct {
+	IDZc   int    `db:"id"`
+	USer   string `db:"name"`
+	PAsswd string `db:"passwd"`
+}
+
 func main() {
 	db, _ := sqlx.Connect("mysql", "root:1q2w3e@tcp(127.0.0.1:3306)/guang?charset=utf8")
 	app := iris.New()
 	app.RegisterView(iris.HTML("./templates", ".html"))
-	// sess := sessions.New(sessions.Config{
-	// 	Cookie:                      "mysessionid",
-	// 	Expires:                     time.Hour * 2,
-	// 	DisableSubdomainPersistence: false,
-	// })
+	sess := sessions.New(sessions.Config{
+		Cookie:                      "mysessionid",
+		Expires:                     time.Hour * 2,
+		DisableSubdomainPersistence: false,
+	})
+	app.StaticWeb("/static", "./static")
+	app.Use(func(ctx iris.Context) {
+		ph := ctx.Path()
+		if ph == "/login" || ph == "/zhuce" {
+			ctx.Next()
+		} else {
+			s := sess.Start(ctx).GetString("name")
+			if s == "" {
+				ctx.Redirect("/static/login.html", iris.StatusTemporaryRedirect)
+			} else {
+				ctx.Next()
+			}
 
+		}
+	})
 	app.Get("/question_list", func(ctx iris.Context) {
+		// s := sess.Start(ctx).GetString("name")
+		// if s != "" {
 		var page float64
 		db.Get(&page, "SELECT count(*) FROM txlyd ")
 		xsys := 10.0
@@ -54,7 +79,7 @@ func main() {
 			aaa = append(aaa, i+1)
 
 		}
-
+		ctx.ViewData("sname", sess.Start(ctx).GetString("name"))
 		ctx.ViewData("sqlnum", aaa)
 		//ctx.FormValue() must don't be null
 		qunayeen := ctx.URLParams()["qunaye"]
@@ -83,9 +108,12 @@ func main() {
 		ctx.ViewData("questiontype_list", questiontypelist)
 		ctx.ViewData("count_list", countlist)
 		ctx.View("question_list.html")
+		// }
+
 	})
 	app.Get("/delete", func(ctx iris.Context) {
-
+		// s := sess.Start(ctx).GetString("name")
+		// if s != "" {
 		deleteidL := ctx.FormValue("deleteid_l")
 		if deleteidL == "" {
 			delid := ctx.FormValue("delid")
@@ -98,14 +126,17 @@ func main() {
 
 			ctx.Redirect("/question_list", iris.StatusTemporaryRedirect)
 		}
+		// }
 
 	})
 	app.Get("/question_moban_update", func(ctx iris.Context) {
+		// s := sess.Start(ctx).GetString("name")
+		// if s != "" {
 		Updateid := ctx.FormValue("updateid")
 		updatesql := []MobaN{}
 		db.Select(&updatesql, "select * from moban where id = ?", Updateid)
 		ctx.ViewData("question_moban_update", updatesql)
-
+		ctx.ViewData("sname", sess.Start(ctx).GetString("name"))
 		wtnrg := ctx.FormValue("wtnrg")
 		wtddg := ctx.FormValue("wtddg")
 		wtysg := ctx.FormValue("wtysg")
@@ -122,10 +153,13 @@ func main() {
 
 		ctx.View("question_moban_update.html")
 
+		// }
+
 	})
 
 	app.Post("/insert", func(ctx iris.Context) {
-
+		// s := sess.Start(ctx).GetString("name")
+		// if s != "" {
 		starttime := ctx.FormValue("starttime")
 		endtime := ctx.FormValue("endtime")
 		question := ctx.FormValue("question")
@@ -148,24 +182,33 @@ func main() {
 			// fmt.Println(starttime, endtime, question, place, questiontype, count, solvetimeS, note)
 			ctx.Redirect("/question_list/?qunaye=", iris.StatusTemporaryRedirect)
 		}
+		// }
 
 	})
 	app.Post("/insert_moban", func(ctx iris.Context) {
+		// s := sess.Start(ctx).GetString("name")
+		// if s != "" {
 		qtlbmb := ctx.FormValue("qtlbmb")
 		qtddmb := ctx.FormValue("qtddmb")
 		wtlx := ctx.FormValue("wtlx")
 		fscs := ctx.FormValue("fscs")
 		// fmt.Println(qtlbmb, qtddmb, wtlx, fscs)
+		ctx.ViewData("sname", sess.Start(ctx).GetString("name"))
 		db.MustExec("insert into moban(question_list,place_list,questiontype_list,count_list) values(?,?,?,?)", qtlbmb, qtddmb, wtlx, fscs)
 		ctx.Redirect("/question_moban/", iris.StatusTemporaryRedirect)
+		// }
+
 	})
 	app.Get("/question_moban", func(ctx iris.Context) {
+		// s := sess.Start(ctx).GetString("name")
+		// if s != "" {
 		questionxianshi := []MobaN{}
 		db.Select(&questionxianshi, "select * from moban")
 		// fmt.Println("moban:", questionxianshi)
 		ctx.ViewData("question_xianshi", questionxianshi)
-
+		ctx.ViewData("sname", sess.Start(ctx).GetString("name"))
 		ctx.View("question_moban.html")
+		// }
 
 	})
 
@@ -173,28 +216,69 @@ func main() {
 		user := ctx.FormValue("user")
 		passwd := ctx.FormValue("passwd")
 		fmt.Println("zhuce:", user, passwd)
-		ctx.Redirect("/question_list/?qunaye=1", iris.StatusTemporaryRedirect)
+		var userpd int
+		db.Get(&userpd, "select count(name) from zhuce where name = ?", user)
+		fmt.Println("userpd:", userpd)
+		if userpd == 0 {
+			s := sess.Start(ctx)
+			s.Set("name", user)
+			db.MustExec("insert into zhuce(name,passwd) values(?,?)", user, passwd)
+			fmt.Println(user, "注册成功")
+			// zccg := user + "注册成功"
+			// ctx.ViewData("zccg", zccg)
+			ctx.Redirect("/question_list/?qunaye=1", iris.StatusTemporaryRedirect)
+		} else {
+			fmt.Println(user, "用户名已存在")
+			zcsb := user + "用户名已存在"
+			ctx.HTML(zcsb)
+		}
+		// ctx.View("zhuce.html")
 	})
 	app.Post("/login", func(ctx iris.Context) {
 		user := ctx.FormValue("lguser")
 		passwd := ctx.FormValue("lgpasswd")
 		fmt.Println("login:", user, passwd)
-		ctx.Redirect("/question_list/?qunaye=1", iris.StatusTemporaryRedirect)
+		var loginc int
+		db.Get(&loginc, "select count(*) from zhuce where name = ? and passwd = ?", user, passwd)
+		fmt.Println("检测用户是否存在", loginc)
+		if loginc == 1 {
+			s := sess.Start(ctx)
+			s.Set("name", user)
+			ctx.Redirect("/question_list/?qunaye=1", iris.StatusTemporaryRedirect)
+		} else {
+			ctx.HTML("用户不存在")
+		}
+
 	})
 	app.Get("/search", func(ctx iris.Context) {
+		// s := sess.Start(ctx).GetString("name")
+		// if s != "" {
 		search := []TxlydSQL{}
 		searchnr := ctx.URLParams()["search_nr"]
 		db.Select(&search, "select * from txlyd where question like  '%"+searchnr+"%'")
 		// fmt.Println("search:", search)
 		ctx.ViewData("search_list", search)
-
+		ctx.ViewData("sname", sess.Start(ctx).GetString("name"))
 		ctx.View("search.html")
+		// }
 
 	})
+	app.Get("/logout", func(ctx iris.Context) {
+		s := sess.Start(ctx).GetString("name")
+		if s != "" {
+			sess.Start(ctx).Delete("name")
+		}
+		ctx.Redirect("/static/login.html", iris.StatusTemporaryRedirect)
+	})
+
 	app.Get("/", func(ctx iris.Context) {
+		// s := sess.Start(ctx).GetString("name")
+		// if s != "" {
 		ctx.Redirect("question_list", iris.StatusTemporaryRedirect)
+		// } else {
+		// ctx.Redirect("/static/login.html", iris.StatusTemporaryRedirect)
+		// }
 	})
 
-	app.StaticWeb("/static", "./static")
 	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
 }
